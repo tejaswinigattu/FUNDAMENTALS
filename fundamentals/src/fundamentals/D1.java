35f42ffcef542d68f75f4a6e72711151b35d0505
Mahesh <Mahesh@LAPTOP-9LDA7L02>
2019-06-24 16:44:20 +0530

    TEJASWINI

---
+++ fundamentals/src/fundamentals/D1.java
@@ -0,0 +1,9 @@
+package fundamentals;
+
+public class D1 {
+	public static void main(String[] args)
+	{
+	System.out.println(args[0]+" Technologies "+args[1]);
+	}
+	
+}
---
+++ fundamentals/src/fundamentals/D2.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class D2 {
+	public static void main(String[] args)
+	{
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[0]);
+	int c=a+b;
+	System.out.println(c);
+	}
+	
+}
