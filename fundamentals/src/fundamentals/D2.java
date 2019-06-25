39ec5389f7f02a3a981b06b241009d2a9a335fc3
Mahesh <Mahesh@LAPTOP-9LDA7L02>
2019-06-25 12:12:45 +0530

    TEJASWINI

--- fundamentals/src/fundamentals/D2.java
+++ fundamentals/src/fundamentals/D2.java
@@ -1,10 +1,10 @@
 package fundamentals;
 
 public class D2 {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
 	int a=Integer.parseInt(args[0]);
-	int b=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
 	int c=a+b;
 	System.out.println(c);
 	}
